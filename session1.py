eab8afa5aa17a1db16a9bbcf3de6f9fdd9c69d09
tomking597 <thomas_ng9889@hotmail.com>
2020-09-04 20:54:41 +1000

    commit sept 4 @1910

---
+++ session1.py
@@ -0,0 +1,17 @@
+print("Learning Objectives
+In this section, you will learn how to:
+    1. Express the value of VSCode and its extensions to COBOL programming/structure
+    2. Recall what the acronym, COBOL, represents
+    3. Identify the different Areas in COBOL code and the numbered columns they contain 
+    4. Recall what a reserved word is in COBOL and examples of those reserved words
+    5. Compare the different program divisions and identify what belongs in each division
+    6. Order the hierarchal levels of COBOL constructs
+    7. Compare implicit and explicit scope terminators
+    8. Recall the rules for a valid Data Name
+    9. Give examples of different variable definitions using the Picture Clause
+
+
+Common business oriented languages
+data processing, ease of reading writing,")
+
+print(" hello world of cobol");
